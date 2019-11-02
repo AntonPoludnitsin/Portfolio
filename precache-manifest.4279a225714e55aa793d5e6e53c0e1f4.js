d16231064c1fa7ed0e7dee8f53a931a7fe98ff9f
unknown <you@example.com>
2019-11-03 02:35:21 +0400

    Updates

---
+++ precache-manifest.4279a225714e55aa793d5e6e53c0e1f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737dad8d05f9dab41f5294802526b4e1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "eda4f6cf3f8c084e4088",
+    "url": "/Portfolio/static/css/main.b52f216b.chunk.css"
+  },
+  {
+    "revision": "1b975f5162868d54b505",
+    "url": "/Portfolio/static/js/2.644baeae.chunk.js"
+  },
+  {
+    "revision": "eda4f6cf3f8c084e4088",
+    "url": "/Portfolio/static/js/main.12cabc54.chunk.js"
+  },
+  {
+    "revision": "63fe68486ecb758e5a56",
+    "url": "/Portfolio/static/js/runtime-main.b0957d76.js"
+  }
+]);
